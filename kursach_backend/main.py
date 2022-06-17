78005fc3e0df019a4234e1e67dbc25e90905ed98
BrigYeet <orest.mokryy@gmail.com>
2022-06-17 21:15:42 +0300

    UPD to kursach

--- kursach_backend/main.py
+++ kursach_backend/main.py
@@ -2,30 +2,48 @@ import datetime
 
 import serial
 from flask import Flask, render_template
-from pymysql import connect
+import psycopg2
+from settings import credentials
 
 app = Flask(__name__)
 
+
+def get_db_connection():
+    connection = psycopg2.connect(dbname=credentials['database_name'],
+                                  user=credentials['database_user'],
+                                  password=credentials['database_password'],
+                                  host=credentials['database_host'])
+
+    return connection
 
 @app.route("/")
 def hello():
     port = serial.Serial('COM3', 9600, timeout=0.1)
     data = str(port.readline().decode('UTF-8'))
+    # print(data)
     if data != '':
         try:
-            with connect(host="localhost",
-                         user="maxym",
-                         password="1527956Makc"
-                         ) as connection:
-                QUERY = "INSERT INTO sensors.sensor_data(value, recorded.at) VALUES (%s, %s)"
-                with connection.cursor() as cursor:
-                    cursor.execute(QUERY, [str(data), datetime.datetime.now()])
-                    connection.commit()
-        except:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            QUERY = "INSERT INTO sensor_data(value) VALUES (%s)"
+
+            cursor.execute(QUERY, (str(data),))
+            connection.commit()
+        except Exception as e:
             print(e)
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    SELECT_QUERY = "SELECT * FROM sensor_data ORDER BY id DESC LIMIT 1"
+    cursor.execute(SELECT_QUERY)
+    result = cursor.fetchone()
+    print(result)
+        # connection.close()
+        # value = result[1]
+
+    return render_template('index.html', data=result[1])
 
 
-    return render_template('index.html', data=data[:5])
 
+    # return '1'
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ kursach_backend/settings.py
@@ -0,0 +1,6 @@
+credentials = {
+'database_name':'kursova',
+'database_user':'orest',
+'database_password':'1234',
+'database_host':'localhost'
+}
